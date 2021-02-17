4161ca4078ba5ee4086e864f1d3d88a898dde272
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2021-02-17 22:11:21 +0000

    finised login function

--- src/Global/Context.js
+++ src/Global/Context.js
@@ -1,13 +1,35 @@
 import React, { createContext, useState } from 'react'
+import { auth } from '../config'
+
 export const ContextProvider = createContext();
+
 
 const Context = (props) => {
     const [model, setModel] = useState(false);
     const openModel = () => {
         setModel(true);
     }
+    const closeModel = () => {
+        setModel(false);
+    }
+    const register = async(user) => {
+        const { username, email, password } = user;
+        try {
+            const res = await auth.createUserWithEmailAndPassword(email, password)
+            res.user.updateProfile({displayName: username})
+            setModel(false)
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    const login = async(user) => {
+        const { email, password } = user
+        const res = await auth.signInWithEmailAndPassword(email, password);
+        setModel(false);
+    }
     return (
-        <ContextProvider.Provider value={{ model, openModel }}>
+        <ContextProvider.Provider value={{ model, openModel, closeModel, register, login }}>
             {props.children}
         </ContextProvider.Provider>
     )
